99e2933de921aff7473be28c30c15ec3c58aa57c
MuzhdaN <muzhda.noorzad122@gmail.com>
2022-07-23 14:05:38 +0000

    add functionality to the objects inside container

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,25 @@ const highscore = document.querySelector(".highscore");
 const highscoreText = document.querySelector(".highscore-text");
 const instructions = document.querySelector('.game-start .rules-btn');
 const homeLink = document.querySelector('.right-side a');
+
+let result = 0;
+let timer;
+let max=0;
+
+for(let i=1; i<=12; i++){
+    let box = document.createElement("div");
+    box.classList.add("box");
+    container.appendChild(box);
+
+    //adding fence image at the bottom of each box
+    let fence = document.createElement("img");
+    fence.classList.add("fence");
+    fence.src = "assets/images/fence.png";
+    box.appendChild(fence);
+
+    let character = document.createElement("img");
+    character.classList.add("character");
+    character.src = "assets/images/Bugs_Bunny.svg.png";
+    character.setAttribute("name", "rab");
+    box.appendChild(character);
+}
