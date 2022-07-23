500d210abb903317c46c7f3e21f8293754716977
MuzhdaN <muzhda.noorzad122@gmail.com>
2022-07-23 14:03:03 +0000

    add varibles at the start of the js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.container');
+const boxes = document.querySelectorAll('.box');
+const timeLeft = document.getElementById("time-left")
+const score = document.querySelector('.score');
+const startBtn = document.querySelector('.modal button');
+const canvas = document.querySelector(".modal");
+const header = document.querySelectorAll('header');
+
+// score
+const highscore = document.querySelector(".highscore");
+const highscoreText = document.querySelector(".highscore-text");
+const instructions = document.querySelector('.game-start .rules-btn');
+const homeLink = document.querySelector('.right-side a');
